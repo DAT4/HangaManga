7c6e65d0a7c9f3a2543a7c0a0c3ef15a1ebcebdc
martin <s195469@student.dtu.dk>
2021-01-05 15:01:09 +0100

    some Activities and fragments added

--- app/src/main/java/com/example/hangamanga/ui/MainActivity.kt
+++ app/src/main/java/com/example/hangamanga/ui/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.hangamanga.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.hangamanga.R
+import com.example.hangamanga.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/hangamanga/ui/fragments/PlayGameFragment.kt
@@ -0,0 +1,27 @@
+package com.example.hangamanga.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hangamanga.R
+import com.example.hangamanga.databinding.FragmentLoginBinding
+
+class PlayGameFragment : Fragment() {
+    private lateinit var _binding: FragmentLoginBinding
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        println("Hello")
+    }
+}
